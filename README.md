95edec07b330837dd0f7516922ab487ccea0b950
minimalic <git@minimalic.com>
2024-04-06 23:29:40 +0200

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# silverstripe-sitetools
-Config fields for SilverStripe
+# SilverStripe SiteTools
+
+Extension for Silverstripe's "siteconfig" for custom settings/fields.
+
+
+## Requirements
+
+* Silverstripe 4 or Silverstripe 5
+
+
+## Installation
+
+```sh
+composer require minimalic/silverstripe-sitetools
+```
+
+Refresh DB (shell website root):
+
+vendor/bin/sake dev/build "flush=all"
+
+
+## Configuration: CMS
+
+Inside CMS use the settings area (tab "Footer").
+
+
+## Configuration: Templates
+
+In your footer templates use:
+
+```html
+$SiteConfig.FooterCopyright
+$SiteConfig.FooterDisclaimer
+```
+
+
+## License
+
+See [License](license.md)
+
+Copyright (c) 2024, minimalic.com - Sebastian Finke
+All rights reserved.
+
---
+++ src/Extensions/SiteConfigExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace minimalic\SiteTools\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+class SiteConfigExtension extends DataExtension
+{
+
+    private static $db = [
+        'FooterCopyright' => 'Varchar',
+        'FooterDisclaimer' => 'HTMLText',
+    ];
+
+    private static $has_one = [
+    ];
+
+    private static $has_many = [
+    ];
+
+    private static $owns = [
+    ];
+
+    private static $defaults = [
+    ];
+
+    public function updateCMSFields(FieldList $fields)
+    {
+
+        $footerCopyright = TextField::create('FooterCopyright', _t(__CLASS__ . '.FooterCopyright', 'Footer copyright text (short) after year (year will be automatically updated)'));
+        $footerTrademarks = HTMLEditorField::create('FooterDisclaimer', _t(__CLASS__ . '.FooterDisclaimer', 'Footer disclaimer text');
+
+        $fields->addFieldsToTab('Root.Footer', array(
+            $footerCopyright,
+            $footerTrademarks,
+        ));
+
+    }
+}
